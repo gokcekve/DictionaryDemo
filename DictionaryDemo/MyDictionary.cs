596a74c722f154c98f01800eb52376c94d292ef1
Gökçe Köküm <gokcekokum@gmail.com>
2021-03-08 22:39:49 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<key, value>
+    {
+        key[] numberPlates;
+        value[] cities;
+
+
+        public MyDictionary()
+        {
+            numberPlates = new key[0];
+            cities = new value[0];
+        }
+
+        public void Add(key numberPlate, value city)
+        {
+            key[] tempArray = numberPlates;
+            value[] tempArray2 = cities;
+
+            numberPlates = new key[numberPlates.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                numberPlates[i] = tempArray[i];
+            }
+
+            cities = new value[cities.Length + 1];
+            for (int j = 0; j < tempArray2.Length; j++)
+            {
+                cities[j] = tempArray2[j];
+            }
+
+            numberPlates[numberPlates.Length - 1] = numberPlate;
+            cities[cities.Length - 1] = city;
+        }
+
+        public int Length
+        {
+            get { return numberPlates.Length; }
+        }
+        public int Length2
+        {
+            get { return cities.Length; }
+        }
+
+        public void ToList()
+        {
+            for (int k = 0; k < numberPlates.Length; k++)
+            {
+                Console.WriteLine(numberPlates[k] + " :  " + cities[k]);
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> plateNo = new MyDictionary<int, string>();
+            plateNo.Add(01, "Adana");
+            plateNo.Add(02, "Adıyaman");
+            plateNo.Add(03, "Afyon");
+            plateNo.Add(04, "Ağrı");
+            plateNo.Add(05, "Amasya");
+            Console.WriteLine(plateNo.Length);
+            plateNo.Add(06, "Ankara");
+            plateNo.Add(07, "Antalya");
+            plateNo.Add(08, "Artvin");
+            plateNo.Add(09, "Aydın");
+            plateNo.Add(10, "Balıkesir");
+            Console.WriteLine(plateNo.Length);
+            plateNo.ToList();
+        }
+    }
+}
